bac251b27213efd4b1ab9df844c9289cabd112d5
ayushmanchhabra <ayushmanchhabra@users.noreply.github.com>
2023-07-06 19:21:03 +0000

    deploy: eba33cc6dc7887038159de72e8d2e0718e130d64

--- assets/mode-get.md.4426cc03.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,o as a,c as n,U as o}from"./chunks/framework.54105999.js";const d=JSON.parse('{"title":"Get NW.js binaries","description":"","frontmatter":{},"headers":[],"relativePath":"mode-get.md","filePath":"mode-get.md"}'),l={name:"mode-get.md"},p=o("",35),e=[p];function t(r,c,D,i,y,F){return a(),n("div",null,e)}const u=s(l,[["render",t]]);export{d as __pageData,u as default};
